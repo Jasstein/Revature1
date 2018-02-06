814f9203761c4ab0998a54fdb177f968f530abf4
JasonSteinberg <jasstein403@gmail.com>
2018-02-06 15:28:22 -0500

    Week5_9

--- PipelineDemo/src/main/java/com/revature/servlets/HelloServlet.java
+++ PipelineDemo/src/main/java/com/revature/servlets/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +16,12 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		PrintWriter pw = response.getWriter();
+		String htmlresponse = "<html>";
+		htmlresponse += "<head>\r\n" + "<meta charset=\"ISO-8859-1\">\r\n" + 
+				"<title>Home Page</title>\r\n" + " <link rel=\"stylesheet\" href=\"static/css/home.css\"> </head>";
+		htmlresponse += "<h2>Welcome.</h2>";
+		htmlresponse += "</html>";
+		pw.println(htmlresponse);
 	}
 }
